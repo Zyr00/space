5fc2c537ff107b5d340c8aa12e9a11cbb0c87d2c
João Cunha <skidrow.jc@gmail.com>
2021-11-05 23:41:35 +0000

    Updated MAX and MIN functions

--- includes/util.h
+++ includes/util.h
@@ -1,7 +1,9 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
-#include <sys/param.h>
+// #include <sys/param.h>
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 int collision(int, int, int, int, int, int, int, int);
 float randomNumber(const float, const float);
